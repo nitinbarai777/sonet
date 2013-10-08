b5fea04838b09604594498ff626d2c2b0b3d1f3c
nitin777 <nitin.barai777@gmail.com>
2013-10-08 23:51:40 +0530

    issue fixed

--- app/controllers/fronts_controller.rb
+++ app/controllers/fronts_controller.rb
@@ -1,17 +1,17 @@
 class FrontsController < ApplicationController
-	require 'builder'
+  require 'builder'
   before_filter :require_user, :only => [:change_password]
   before_filter :set_header_menu_active
   
   #dashboard
   def dashboard
-  	if current_user
-  	  if is_user?
-  			if current_user.user_urls
-  			  redirect_to contents_url
-  			end
-  		end	
-		end
+    if current_user
+      if is_user?
+        if current_user.user_urls
+          redirect_to contents_url
+        end
+      end 
+    end
   end
   
   #user login
@@ -84,8 +84,8 @@ class FrontsController < ApplicationController
           Twitter.configure do |config|
             config.consumer_key = TWITTER_CONSUMER_KEY
             config.consumer_secret = TWITTER_CONSUMER_SECRET
-            config.oauth_token = session[:twittertoken]
-            config.oauth_token_secret = session[:twittersecret]
+            config.oauth_token = params[:twittertoken]
+            config.oauth_token_secret = params[:twittersecret]
           end
           Twitter.update(contentselected)
           objData = {} 
@@ -117,85 +117,85 @@ class FrontsController < ApplicationController
     render :layout => "news"
   end
 
-	#forgot password
+  #forgot password
   def forgot_password
-		@user = User.new
-		if params[:user]
-			if user = authenticate_email(params[:user][:email])
-				new_pass = SecureRandom.hex(5)
-				user.password = user.password_confirmation = new_pass
-				user.save
-				body = render_to_string(:partial => "common/forgot_password_mail", :locals => { :username => user.username, :new_pass => new_pass }, :formats => [:html])
-				body = body.html_safe
-				UserMailer.forgot_password_confirmation(user.email, new_pass, body).deliver
-				@user_session = UserSession.find
-				if @user_session
-					@user_session.destroy
-				end
-				session[:user_id] = nil
-				flash[:notice] = t("general.password_has_been_sent_to_your_email_address")
-				redirect_to root_path 
-			else
-				flash[:forgot_pass_error] = t("general.no_user_exists_for_provided_email_address")
-				redirect_to :action => "forgot_password"
-			end
-		end	
+    @user = User.new
+    if params[:user]
+      if user = authenticate_email(params[:user][:email])
+        new_pass = SecureRandom.hex(5)
+        user.password = user.password_confirmation = new_pass
+        user.save
+        body = render_to_string(:partial => "common/forgot_password_mail", :locals => { :username => user.username, :new_pass => new_pass }, :formats => [:html])
+        body = body.html_safe
+        UserMailer.forgot_password_confirmation(user.email, new_pass, body).deliver
+        @user_session = UserSession.find
+        if @user_session
+          @user_session.destroy
+        end
+        session[:user_id] = nil
+        flash[:notice] = t("general.password_has_been_sent_to_your_email_address")
+        redirect_to root_path 
+      else
+        flash[:forgot_pass_error] = t("general.no_user_exists_for_provided_email_address")
+        redirect_to :action => "forgot_password"
+      end
+    end 
   end
 
-	#change password
+  #change password
   def change_password
-  	@o_single = User.find(current_user.id)
-  	if params[:user]
-		  @o_single.password = params[:user][:password]
-		  @o_single.password_confirmation = params[:user][:password_confirmation]
-		  @o_single.password_required = true
-	    respond_to do |format|
-	      if @o_single.update_attributes(user_params)
-	        format.html { redirect_to contents_url, notice: t("general.successfully_updated") }
-	        format.json { head :no_content }
-	      else
-	        format.html { render action: 'change_password' }
-	        format.json { render json: @o_single.errors, status: :unprocessable_entity }
-	      end
-	    end
-	  end  
+    @o_single = User.find(current_user.id)
+    if params[:user]
+      @o_single.password = params[:user][:password]
+      @o_single.password_confirmation = params[:user][:password_confirmation]
+      @o_single.password_required = true
+      respond_to do |format|
+        if @o_single.update_attributes(user_params)
+          format.html { redirect_to contents_url, notice: t("general.successfully_updated") }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'change_password' }
+          format.json { render json: @o_single.errors, status: :unprocessable_entity }
+        end
+      end
+    end  
   end
   
- 	#authenticate user though linkedin network
+  #authenticate user though linkedin network
   def auth_login
-		auth_hash = request.env['omniauth.auth']
-		auth_response = Authorization.find_or_create(auth_hash)
-		# Create a session
-		user = auth_response.user
-		session[:user_id] = user.id
-		session[:token] = auth_response.token
-		session[:secret] = auth_response.secret
-		session[:user_role] = USER
-		user_session = UserSession.create(user)
-		user_session.save
-		if session[:extension]
-		  session[:extension] = nil
-		  objData = {token: session[:token], secret: session[:secret], email: user.email}
+    auth_hash = request.env['omniauth.auth']
+    auth_response = Authorization.find_or_create(auth_hash)
+    # Create a session
+    user = auth_response.user
+    session[:user_id] = user.id
+    session[:token] = auth_response.token
+    session[:secret] = auth_response.secret
+    session[:user_role] = USER
+    user_session = UserSession.create(user)
+    user_session.save
+    if session[:extension]
+      session[:extension] = nil
+      objData = {token: session[:token], secret: session[:secret], email: user.email}
       render json: {
         success: {
           data: objData,
           message:  'token and secret keys',
           status_code: 200
         }
-      }		  
-		else
-		  flash.keep[:notice] = t("general.login_successful")
-		  redirect_to root_url
-		end  
+      }     
+    else
+      flash.keep[:notice] = t("general.login_successful")
+      redirect_to root_url
+    end  
   end
   
-	#footer and other static pages
+  #footer and other static pages
   def other
-  	@o_single = StaticPage.where(page_route: params[:fp]).first
+    @o_single = StaticPage.where(page_route: params[:fp]).first
   end
   
 
-	private
+  private
 
   def set_header_menu_active
     @dashboard = true
